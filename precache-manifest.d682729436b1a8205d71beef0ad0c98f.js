0bee81b7ea561469d26c64393874c316c12e560f
tamara96mc <tamara.96mc@gmail.com>
2020-12-08 12:55:50 +0100

    Updates

---
+++ precache-manifest.d682729436b1a8205d71beef0ad0c98f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6b139f0096992a0f77ffd6a78f48245",
+    "url": "/cotizador/index.html"
+  },
+  {
+    "revision": "b93f1bde5a70c8c9f2da",
+    "url": "/cotizador/static/css/main.c52a1e4c.chunk.css"
+  },
+  {
+    "revision": "8e8273028572f29b03b4",
+    "url": "/cotizador/static/js/2.2764e7bd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/cotizador/static/js/2.2764e7bd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b93f1bde5a70c8c9f2da",
+    "url": "/cotizador/static/js/main.213f07a0.chunk.js"
+  },
+  {
+    "revision": "ea6376e55e96807a78bb",
+    "url": "/cotizador/static/js/runtime-main.a3ceb00a.js"
+  }
+]);
